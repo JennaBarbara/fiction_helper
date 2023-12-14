ba5a4b13d707292ff1a784e8b0f35e504dfd3118
Jenna Barbara Christensen <jennabarbara@gmail.com>
2023-12-14 16:58:21 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+import './App.css';
+
+const App = () => (
+    <div className="App">
+
+    </div>
+);
+
+export default hot(module)(App);
